6a644519bf3306add01f57a192d7d3ef2de48deb
Herve Pages <hpages@fhcrc.org>
2008-05-08 21:53:57 +0000

    Resynced with trunk version (svn merge -r 31546:31576 https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome) in order to merge the current work on the masking feature. Version bumped to 1.8.1
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_2/madman/Rpacks/BSgenome@31581 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/BSgenomeForge.R
@@ -0,0 +1,105 @@
+###
+###
+
+forgeSeqFiles <- function(srcdir, destdir, names, prefix="", suffix="", comments=NULL, single.seq=TRUE)
+{
+    if (length(names) == 0)
+        return()
+    for (i in 1:length(names)) {
+        name <- names[i]
+        srcfile <- paste(prefix, name, suffix, sep="")
+        srcpath <- paste(srcdir, srcfile, sep="/")
+        cat("Loading FASTA file '", srcpath, "' in '", name, "' object... ", sep="")
+        seq <- read.DNAStringSet(srcpath, "fasta")
+        cat("DONE\n")
+        if (single.seq) {
+            if (length(seq) != 1)
+                stop("file should contain exactly one sequence, found ", length(seq))
+            seq <- seq[[1]] # now 'seq' is a DNAString object
+        }
+        if (is.null(comments))
+            c <- ""
+        else
+            c <- comments[i]
+        comment(seq) <- paste(c, " (generated from FASTA file ", srcfile, ")", sep="")
+        assign(name, seq)
+        dest <- paste(destdir, "/", name, ".rda", sep="")
+        cat("Saving '", name, "' object to compressed data file '", dest, "'... ", sep="")
+        save(list=name, file=dest, compress=TRUE)
+        cat("DONE\n")
+        remove(list=name)
+    }
+}
+
+## mask1 is the mask of "inter-contig gaps".
+.makeMask1 <- function(srcdir, seqname, seq)
+{
+    file <- file.path(srcdir, "liftAll.lft")
+    if (file.exists(file)) {
+        ans <- read.liftMask(file, seqname=seqname, width=length(seq))
+    } else {
+        ## If the liftAll.lft file is missing, the fallback is to mask
+        ## N-blocks of width >= 4
+        ans <- maskMotif(seq, "N", min.block.width=4)
+    }
+    ans
+}
+
+## mask2 is the "rm" mask (from the RepeatMasker .out file).
+.makeMask2 <- function(srcdir, seqname, mask_width)
+{
+    file <- file.path(srcdir, paste(seqname, ".fa.out", sep=""))
+    if (file.exists(file)) {
+        ans <- read.rmMask(file, mask_width)
+        names(ans) <- "RepeatMasker"
+    } else {
+        ans <- Mask(mask_width, start=integer(0), width=integer(0))
+        names(ans) <- "RepeatMasker (empty)"
+    }
+    ans
+}
+
+## mask3 is the "trf" mask (from the Tandem Repeats Finder .bed file).
+.makeMask3 <- function(srcdir, seqname, mask_width)
+{
+    file <- file.path(srcdir, paste(seqname, ".bed", sep=""))
+    if (file.exists(file)) {
+        ans <- read.trfMask(file, mask_width)
+        names(ans) <- "Tandem Repeats Finder (period<=12)"
+    } else {
+        ans <- Mask(mask_width, start=integer(0), width=integer(0))
+        names(ans) <- "Tandem Repeats Finder (empty)"
+    }
+    ans
+}
+
+forgeMaskFiles <- function(srcdir, destdir, names, seqdir)
+{
+    for (seqname in names) {
+        ## Get the length of the sequence.
+        seqfile <- file.path(seqdir, paste(seqname, ".rda", sep=""))
+        load(seqfile)
+        mask_width <- length(get(seqname))
+
+        ## Start with an empty mask collection (i.e. a MaskCollection of
+        ## length 0).
+        masks <- new("MaskCollection", width=mask_width)
+        mask1 <- .makeMask1(srcdir, seqname, get(seqname))
+        remove(list=seqname)
+        masks <- append(masks, mask1)
+        mask2 <- .makeMask2(srcdir, seqname, mask_width)
+        masks <- append(masks, mask2)
+        mask3 <- .makeMask3(srcdir, seqname, mask_width)
+        masks <- append(masks, mask3)
+
+        ## Save the masks.
+        objname <- paste("masks.", seqname, sep="")
+        assign(objname, masks)
+        dest <- file.path(destdir, paste(objname, ".rda", sep=""))
+        cat("Saving '", objname, "' object to compressed data file '", dest, "'... ", sep="")
+        save(list=objname, file=dest, compress=TRUE)
+        cat("DONE\n")
+        remove(list=objname)
+    }
+}
+
